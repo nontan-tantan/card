d06ad14c2040be3baea01bd4c4e84ff32dc1e2b2
藤岡希美 <longestyard851@gmail.com>
2026-07-15 19:22:31 +0900

    はじめてのコミット

---
+++ script.js
@@ -0,0 +1,14 @@
+const messages = [
+  "シマエナガが大好きです！🐤",
+  "最近AIにハマっています🧠",
+  "趣味はバドミントンです🏸",
+  "今日も元気にプログラミング！💻",
+  "よろしくお願いします😊！"
+];
+
+let current = 0;
+
+document.getElementById("change-btn").addEventListener("click", function() {
+  current = (current + 1) % messages.length;
+  document.querySelector(".message").textContent = messages[current];
+});
